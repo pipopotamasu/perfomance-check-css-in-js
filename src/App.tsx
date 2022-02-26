b160f3217530e81bdf22977ebea9233a2d893c3a
pipopotamasu <ec0156hx@gmail.com>
2022-02-26 15:13:55 +0900

    add performance check code (#1)
    * performance check code
    
    * fix jest.config.js
    
    * rm jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div``;
+
+const COMPONENTS_COUNT = 10000;
+
+function StyledComponent() {
+  const divs = [...Array(COMPONENTS_COUNT)].map((_, i) => <StyledDiv key={i}>{i + 1}</StyledDiv>);
+  return <>{divs}</>;
+}
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// @ts-ignore
+function PureComponent() {
+  const divs = [...Array(COMPONENTS_COUNT)].map((_, i) => <div key={i}>{i + 1}</div>);
+  return <>{divs}</>;
+}
+/* eslint-enable */
+
 function App() {
-  return <div></div>;
+  const [render, setRender] = useState(false);
+  const onClick = () => {
+    setRender(true);
+  };
+
+  return (
+    <>
+      <button onClick={onClick}>render start</button>
+      {render && <StyledComponent />}
+    </>
+  );
 }
 
 export default App;
