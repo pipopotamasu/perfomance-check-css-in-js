9c0577b3e1e364670cfa5f39a9418c7da22bd207
pipopotamasu <ec0156hx@gmail.com>
2022-02-26 15:06:42 +0900

    fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,6 @@ module.exports = {
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleDirectories: ['node_modules', './src'],
   rootDir: './src',
-  transform: {
-    '^.+\\.css$': '<rootDir>/tests/config/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/tests/config/fileTransform.js',
-  },
-  setupFilesAfterEnv: ['<rootDir>/tests/config/setupTest.ts'],
   moduleNameMapper: {
     '^@/(.+)': '<rootDir>/$1',
   },
